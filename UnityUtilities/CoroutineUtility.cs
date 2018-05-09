3212e35c9deca53e91bfde8bf64a4b20cacc58c0
Bruno Silva Freire <brunorbsf@gmail.com>
2018-05-09 12:27:57 -0300

    Adicionado CoroutineUtility.cs

---
+++ UnityUtilities/CoroutineUtility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UnityUtilities {
+    public static class CoroutineUtility {
+        public static void ReplaceCoroutine(ref Coroutine coroutine, MonoBehaviour owner, IEnumerator newRoutine) {
+            coroutine.StopIfNotNull(owner);
+            coroutine = owner.StartCoroutine(newRoutine);
+        }
+
+        public static void StopIfNotNull(this Coroutine coroutine, MonoBehaviour owner) {
+            if (coroutine != null) {
+                coroutine.Stop(owner);
+            }
+        }
+
+        public static void Stop(this Coroutine coroutine, MonoBehaviour owner) {
+            owner.StopCoroutine(coroutine);
+        }
+    }
+}
