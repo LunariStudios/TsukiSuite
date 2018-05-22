115e0c06fae5a9e54e3aaae9d62d2a953d2ab356
DDevilISL <brunorbsf@gmail.com>
2018-05-21 23:21:40 -0300

    Added UIUtility

---
+++ UnityUtilities/UIUtility.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UI;
+
+namespace UnityUtilities {
+    public static class UIUtility {
+        public static void SetAlpha(this Graphic graphic, float alpha) {
+            var c = graphic.color;
+            c.a = alpha;
+            graphic.color = c;
+        }
+    }
+}
