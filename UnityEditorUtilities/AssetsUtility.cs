8091e3a2f004c3a7033411080aa70ff90b12dc1b
DDevilISL <brunorbsf@gmail.com>
2018-06-24 19:09:24 -0300

    - Adicionado projeto UnityEditorUtilities

---
+++ UnityEditorUtilities/AssetsUtility.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtilities.Editor {
+    public static class AssetsUtility {
+        public static void AddToAssetFile<T>(this ScriptableObject obj, bool save = true) where T : ScriptableObject {
+            var o = ScriptableObject.CreateInstance<T>();
+            AssetDatabase.AddObjectToAsset(o, obj);
+            if (save) {
+                AssetDatabase.SaveAssets();
+            }
+        }
+
+        public static void CreateAsset(string path, Object asset) {
+            var assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New Cutscene.asset");
+            AssetDatabase.CreateAsset(asset, assetPathAndName);
+            AssetDatabase.SaveAssets();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+        }
+
+        public static T CreateAsset<T>(string path) where T : ScriptableObject {
+            var obj = ScriptableObject.CreateInstance<T>();
+            CreateAsset(path, obj);
+            return obj;
+        }
+    }
+}
