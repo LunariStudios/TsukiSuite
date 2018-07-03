faa98fde263a0d1db9f188a79324f48b4c3c084f
Bruno Silva Freire <brunorbsf@gmail.com>
2018-07-03 18:53:44 -0300

    - Adicionado funções de utilidade para Assets - Adicionado nuget spec

--- UnityEditorUtilities/AssetsUtility.cs
+++ UnityEditorUtilities/AssetsUtility.cs
@@ -1,14 +1,28 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace UnityUtilities.Editor {
     public static class AssetsUtility {
-        public static void AddToAssetFile<T>(this ScriptableObject obj, bool save = true) where T : ScriptableObject {
-            var o = ScriptableObject.CreateInstance<T>();
+        public static T AddToAssetFile<T>(this ScriptableObject obj, bool setDirty = true, bool save = true)
+            where T : ScriptableObject {
+            return (T) obj.AddToAssetFile(typeof(T), setDirty, save);
+        }
+
+        public static Object AddToAssetFile(this ScriptableObject obj, Type type, bool setDirty = true,
+            bool save = true) {
+            var o = ScriptableObject.CreateInstance(type);
             AssetDatabase.AddObjectToAsset(o, obj);
+            if (setDirty) {
+                EditorUtility.SetDirty(obj);
+            }
+
             if (save) {
                 AssetDatabase.SaveAssets();
             }
+
+            return o;
         }
 
         public static void CreateAsset(string path, Object asset) {
