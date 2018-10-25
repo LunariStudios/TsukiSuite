6701181af3d33b5a356566e889823fb4d9c67f7c
Bruno Silva Freire <brunorbsf@gmail.com>
2018-10-25 10:23:19 -0300

    - Updated Assembly Info and Project files for proper versioning

--- UnityUtilities/Properties/AssemblyInfo.cs
+++ UnityUtilities/Properties/AssemblyInfo.cs
@@ -1,34 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("UnityUtilities")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("A set of libraries created for improving workflow on the Unity Engine")]
+[assembly: AssemblyCompany("LAJE Studio")]
 [assembly: AssemblyProduct("UnityUtilities")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("CD6BFA86-1DAE-4E6D-99EA-7EED02389F2B")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
