d918e6b127db16f2f85afbbd55c25875b8cb22fb
Bruno Silva Freire <brunorbsf@gmail.com>
2019-01-09 17:49:32 -0200

    Finished rebranding

--- Scripts/pack.bat
+++ Scripts/pack.bat
@@ -1,5 +1,5 @@
 call load
 mkdir %BUILD_LOCATION%
 del %BUILD_LOCATION%\*.nupkg
-nuget pack %ROOT_LOCATION%\UnityUtilities\UnityUtilities.csproj -Build -OutputDirectory %BUILD_LOCATION%
-nuget pack %ROOT_LOCATION%\UnityEditorUtilities\UnityEditorUtilities.csproj -Build -OutputDirectory %BUILD_LOCATION%
+nuget pack %ROOT_LOCATION%\Tsuki-Runtime\Tsuki-Runtime.csproj -Build -OutputDirectory %BUILD_LOCATION%
+nuget pack %ROOT_LOCATION%\Tsuki-Editor\Tsuki-Editor.csproj -Build -OutputDirectory %BUILD_LOCATION%
