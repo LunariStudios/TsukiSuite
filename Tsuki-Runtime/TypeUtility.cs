7305df3f0da1423734d580c954c6925252798c83
Bruno Silva Freire <brunorbsf@gmail.com>
2019-01-11 14:21:19 -0200

    Adding new extensions for types

--- Tsuki-Runtime/TypeUtility.cs
+++ Tsuki-Runtime/TypeUtility.cs
@@ -1,9 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace Lunari.Tsuki {
     public static class TypeUtility {
+        public static T GetCustomAttribute<T>(this Type type, bool inherit = false) where T : Attribute {
+            var attr = type.GetCustomAttributes(inherit);
+            foreach (var o in attr) {
+                if (o is T attribute) {
+                    return attribute;
+                }
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<T> GetCustomAttributes<T>(this Type type, bool inherit = false) where T : Attribute {
+            var attr = type.GetCustomAttributes(inherit);
+            foreach (var o in attr) {
+                if (o is T attribute) {
+                    yield return attribute;
+                }
+            }
+        }
+
+        public static readonly Dictionary<string, string> Wrapped2Primitive = new Dictionary<string, string> {
+            {"Single", "float"},
+            {"Double", "double"},
+            {"SByte", "sbyte"},
+            {"Int16", "short"},
+            {"Int32", "int"},
+            {"Int64", "long"},
+            {"Byte", "byte"},
+            {"UInt16", "ushort"},
+            {"UInt32", "uint"},
+            {"UInt64", "ulong"},
+            {"Decimal", "decimal"},
+            {"String", "string"},
+            {"Char", "char"},
+            {"Boolean", "bool"},
+            {"Single[]", "float[]"},
+            {"Double[]", "double[]"},
+            {"SByte[]", "sbyte[]"},
+            {"Int16[]", "short[]"},
+            {"Int32[]", "int[]"},
+            {"Int64[]", "long[]"},
+            {"Byte[]", "byte[]"},
+            {"UInt16[]", "ushort[]"},
+            {"UInt32[]", "uint[]"},
+            {"UInt64[]", "ulong[]"},
+            {"Decimal[]", "decimal[]"},
+            {"String[]", "string[]"},
+            {"Char[]", "char[]"},
+            {"Boolean[]", "bool[]"},
+        };
+
+        private static string CheckPrimitive(this Type type) {
+            var typeName = type.Name;
+            var altTypeName = string.Empty;
+
+            if (Wrapped2Primitive.TryGetValue(typeName, out altTypeName)) {
+                typeName = altTypeName;
+            }
+
+            return typeName;
+        }
+
+        public static bool InheritsFrom(this Type type, Type baseType) {
+            if (baseType.IsAssignableFrom(type)) {
+                return true;
+            }
+
+            if (type.IsInterface && baseType.IsInterface == false) {
+                return false;
+            }
+
+            if (baseType.IsInterface) {
+                return type.GetInterfaces().Any(tt => baseType == tt);
+            }
+
+            var t = type;
+            while (t != null) {
+                if (t == baseType) {
+                    return true;
+                }
+
+                if (baseType.IsGenericTypeDefinition && t.IsGenericType && t.GetGenericTypeDefinition() == baseType) {
+                    return true;
+                }
+
+                t = t.BaseType;
+            }
+
+            return false;
+        }
+
+        public static string GetLegibleName(this Type type) {
+            if (type.IsArray) {
+                int rank = type.GetArrayRank();
+                return type.GetElementType().GetLegibleName() + (rank == 1 ? "[]" : "[,]");
+            }
+
+            if (type.InheritsFrom(typeof(Nullable<>))) {
+                return type.GetGenericArguments()[0].GetLegibleName() + "?";
+            }
+
+            if (type.IsByRef) {
+                return "ref " + type.GetElementType().GetLegibleName();
+            }
+
+            if (type.IsGenericParameter || !type.IsGenericType) {
+                return CheckPrimitive(type);
+            }
+
+            var builder = new StringBuilder();
+            var name = type.Name;
+            var index = name.IndexOf("`");
+
+            if (index != -1) {
+                builder.Append(name.Substring(0, index));
+            } else {
+                builder.Append(name);
+            }
+
+            builder.Append('<');
+            var args = type.GetGenericArguments();
+
+            for (int i = 0; i < args.Length; i++) {
+                var arg = args[i];
+
+                if (i != 0) {
+                    builder.Append(", ");
+                }
+
+                builder.Append(GetLegibleName(arg));
+            }
+
+            builder.Append('>');
+            return builder.ToString();
+        }
+
         private static readonly List<Assembly> KnownAssemblies = new List<Assembly>();
 
         private static void Reload() {
