61b8fad69671e8fab08de4ddb679515a37cd3af7
Bruno Silva Freire <brunorbsf@gmail.com>
2019-03-04 19:30:35 -0300

    > v2.0.0

--- Tsuki-Editor/Properties/AssemblyInfo.cs
+++ Tsuki-Editor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Lunari Studios")]
 [assembly: AssemblyProduct("Tsuki-Editor")]
 [assembly: AssemblyCopyright("Copyright © 2019")]
-[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyVersion("2.0.0")]
 [assembly: ComVisible(false)]
 
 [assembly: Guid("514403C5-C91E-441F-9B99-8732947A6238")]
