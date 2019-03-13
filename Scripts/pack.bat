73f9773d0224f2c4726a6ea077de8dda1f2a0a34
Bruno Silva Freire <brunorbsf@gmail.com>
2019-03-13 13:18:30 -0300

    > Moved CategoryAttribute to Tsuki-Runtime > pack.bat no longer build the project

--- Scripts/pack.bat
+++ Scripts/pack.bat
@@ -1,5 +1,5 @@
 call load
 mkdir %BUILD_LOCATION%
 del %BUILD_LOCATION%\*.nupkg
-nuget pack %ROOT_LOCATION%\Tsuki-Runtime\Tsuki-Runtime.csproj -Build -Properties Configuration=Release -Properties Platform=AnyCPU -OutputDirectory %BUILD_LOCATION%
-nuget pack %ROOT_LOCATION%\Tsuki-Editor\Tsuki-Editor.csproj -Build -Properties Configuration=Release -Properties Platform=AnyCPU -OutputDirectory %BUILD_LOCATION%
+nuget pack %ROOT_LOCATION%\Tsuki-Runtime\Tsuki-Runtime.csproj -Properties Configuration=Release -Properties Platform=AnyCPU -OutputDirectory %BUILD_LOCATION%
+nuget pack %ROOT_LOCATION%\Tsuki-Editor\Tsuki-Editor.csproj -Properties Configuration=Release -Properties Platform=AnyCPU -OutputDirectory %BUILD_LOCATION%
