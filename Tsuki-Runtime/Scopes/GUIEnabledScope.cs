c2cc16aad31119f587ed1fffa3085d85f027accf
Bruno Silva Freire <brunorbsf@gmail.com>
2019-10-07 21:32:08 -0300

    > v2.1.0

---
+++ Tsuki-Runtime/Scopes/GUIEnabledScope.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Lunari.Tsuki.Scopes {
+    public class GUIEnabledScope : IDisposable {
+        private bool before;
+
+        public GUIEnabledScope(bool enabled) {
+            before = GUI.enabled;
+            GUI.enabled = enabled;
+        }
+
+        public void Dispose() {
+            GUI.enabled = before;
+        } 
+    }
+}
