9442a83e39d55fac7890c89c16209c90350ec850
Bruno Silva Freire <brunorbsf@gmail.com>
2019-12-07 13:58:08 -0300

    > Added Graphs to Build Script

--- Scripts/pack.bat
+++ Scripts/pack.bat
@@ -3,3 +3,4 @@ mkdir %BUILD_LOCATION%
 del %BUILD_LOCATION%\*.nupkg
 nuget pack %ROOT_LOCATION%\Tsuki-Runtime\Tsuki-Runtime.csproj -Properties Configuration=Release -Properties Platform=AnyCPU -OutputDirectory %BUILD_LOCATION%
 nuget pack %ROOT_LOCATION%\Tsuki-Editor\Tsuki-Editor.csproj -Properties Configuration=Release -Properties Platform=AnyCPU -OutputDirectory %BUILD_LOCATION%
+nuget pack %ROOT_LOCATION%\Tsuki-Graphs\Tsuki-Graphs.csproj -Properties Configuration=Release -Properties Platform=AnyCPU -OutputDirectory %BUILD_LOCATION%
