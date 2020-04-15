151c723c2274aca82fd4359d7f8487ed3f043473
Bruno Silva Freire <brunorbsf@gmail.com>
2020-04-15 11:06:10 -0300

    -Add unityeditor to the editors

--- Assets/Editor/AssetsUtility.cs
+++ Assets/Editor/AssetsUtility.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -40,3 +41,4 @@ namespace Lunari.Tsuki.Editor {
         }
     }
 }
+#endif
