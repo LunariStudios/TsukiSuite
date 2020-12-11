b21c642bdec216f892cae76e54aa05bc60602e9b
BrunoSilvaFreire <brunorbsf@gmail.com>
2020-12-11 19:22:33 -0300

    > Update Graphs classes naming to better reflect implementation > Add tests for Graphs module > Update GUI Styles

---
+++ Assets/Graphs/Exceptions/VertexOutOfRangeException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lunari.Tsuki.Graphs.Exceptions {
+    public class VertexOutOfRangeException<V, E> : Exception {
+        public VertexOutOfRangeException(
+            int index,
+            Graph<V, E> graph
+        ) : base($"Vertex {index} is out of range of graph {graph} (Has {graph.Size} vertices)") { }
+    }
+}
