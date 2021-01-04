b8de4d7f139808f03a6c40c020746ee6b4721923
Bruno Silva Freire <brunorbsf@gmail.com>
2021-01-04 18:00:29 -0300

    > Entity System > Refactor TypeSelectors

--- Assets/Editor/StylesGenerator.cs
+++ Assets/Editor/StylesGenerator.cs
@@ -1,18 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using UnityEditor;
 using UnityEngine;
 
 namespace Lunari.Tsuki.Editor {
-    public class GUIStylesGenerator {
-        public const string GUIStylesPath = "Assets/Editor/GUIStyles.cs";
+    public static class StylesGenerator {
+        public const string StylesPath = "Assets/Editor/Styles.cs";
 
         private static void AddSkinText(StringBuilder builder, string key, EditorSkin skin) {
             builder.Append("public static GUIStyle ");
             builder.Append(key);
-            builder.Append("_");
-            builder.Append(skin.ToString());
-            builder.Append(" => Get(");
+            builder.Append(" => Styles.Get(Keys.");
             builder.Append(key);
             builder.Append(", EditorSkin.");
             builder.Append(skin);
@@ -31,29 +31,50 @@ namespace Lunari.Tsuki.Editor {
                 .AppendLine("// ReSharper disable InconsistentNaming")
                 .AppendLine()
                 .AppendLine("namespace Lunari.Tsuki.Editor {")
-                .AppendLine("public static class GUIStyles {")
+                .AppendLine("public static class Styles {")
                 .AppendLine("public static GUIStyle Get(string key, EditorSkin skin = EditorSkin.Inspector) {")
                 .AppendLine("return EditorGUIUtility.GetBuiltinSkin(skin).FindStyle(key);")
                 .AppendLine("}");
-            foreach (var o in EditorGUIUtility.GetBuiltinSkin(EditorSkin.Inspector)) {
-                var style = (GUIStyle) o;
-                str.Append("public const string ");
+            var fieldNameToKey = new Dictionary<string, string>();
+            var guiSkins = EditorGUIUtility.GetBuiltinSkin(EditorSkin.Inspector);
+            foreach (var o in guiSkins) {
+                var style = (GUIStyle)o;
                 var fieldName = style.name.Replace(' ', '_').Replace('.', '_');
-                str.Append(fieldName);
+                fieldNameToKey[fieldName] = style.name;
+            }
+            foreach (var pair in fieldNameToKey) {
+                str.Append("public static GUIStyle ");
+                str.Append(pair.Key);
+                str.Append(" => ");
+                str.Append("Keys.");
+                str.Append(pair.Key);
+                str.AppendLine(";");
+            }
+            str.AppendLine("public static class Keys {");
+            foreach (var pair in fieldNameToKey){
+                str.Append("public const string ");
+                str.Append(pair.Key);
                 str.Append(" = ");
                 str.Append('"');
-                str.Append(style.name);
+                str.Append(pair.Value);
                 str.Append('"');
                 str.AppendLine(";");
-                AddSkinText(str, fieldName, EditorSkin.Game);
-                AddSkinText(str, fieldName, EditorSkin.Inspector);
-                AddSkinText(str, fieldName, EditorSkin.Scene);
+            }
+            str.AppendLine("}");
+            foreach (var editorSkin in new[] {
+                EditorSkin.Game, EditorSkin.Inspector, EditorSkin.Scene
+            }) {
+                str.AppendLine($"public static class {editorSkin} {{");
+                foreach (var o in fieldNameToKey) {
+                    AddSkinText(str, o.Key, editorSkin);
+                }
+                str.AppendLine("}");
             }
 
             str.AppendLine("}");
             str.AppendLine("}");
             str.AppendLine("#endif");
-            var finalPath = Path.GetFullPath(GUIStylesPath);
+            var finalPath = Path.GetFullPath(StylesPath);
             Debug.Log($"Generated to {finalPath}");
             File.WriteAllText(finalPath, str.ToString());
         }
