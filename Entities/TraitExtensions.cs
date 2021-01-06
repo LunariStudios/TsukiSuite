f3baa8c02530af0e269147af2ba21a7edef8a6a2
BrunoSilvaFreire <brunorbsf@gmail.com>
2021-01-06 17:33:37 -0300

    > Add HasTrait extension

--- Entities/TraitExtensions.cs
+++ Entities/TraitExtensions.cs
@@ -1,5 +1,9 @@
 namespace Lunari.Tsuki.Entities {
     public static class TraitExtensions {
+        public static bool HasTrait<T>(this ITraits traits) where T : Trait {
+            return traits.GetTrait<T>() != null;
+        }
+
         public static bool Access<T>(this ITraits traits, out T trait) where T : Trait {
             return (trait = traits.GetTrait<T>()) != null;
         }
