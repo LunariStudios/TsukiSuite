00e990a2aabb7e91a0c6c50b911f36258c2dfe67
BrunoSilvaFreire <brunorbsf@gmail.com>
2021-01-06 18:16:11 -0300

    > Added GetTraits

--- Assets/Entities/ITraits.cs
+++ Assets/Entities/ITraits.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+
 namespace Lunari.Tsuki.Entities {
     public interface ITraits {
         T GetTrait<T>() where T : Trait;
+        IEnumerable<T> GetTraits<T>() where T : Trait;
     }
 }
