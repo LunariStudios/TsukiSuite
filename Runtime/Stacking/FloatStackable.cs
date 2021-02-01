5eb12c959b87125645010a998f7eb35e3a30fc06
BrunoSilvaFreire <brunorbsf@gmail.com>
2021-02-01 17:37:05 -0300

    > Added stackable values

---
+++ Runtime/Stacking/FloatStackable.cs
@@ -0,0 +1,31 @@
+using System;
+namespace Lunari.Tsuki.Runtime.Stacking {
+    [Serializable]
+    public class FloatStackable : Stackable<float> {
+        public float masterMultiplier = 1;
+        public override float Value => baseValue * Multiplier;
+
+        public float Multiplier {
+            get {
+                var result = masterMultiplier;
+                if (modifiers != null) {
+                    foreach (var multiplierHandle in modifiers) {
+                        result *= multiplierHandle.value;
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        public static implicit operator float(FloatStackable property) {
+            return property.Value;
+        }
+
+        public static implicit operator FloatStackable(float value) {
+            return new FloatStackable {
+                baseValue = value
+            };
+        }
+    }
+}
---
+++ Runtime/Stacking/Stackable.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lunari.Tsuki.Runtime.Stacking {
+    public class Modifier<T> {
+        public T value;
+    }
+    [Serializable]
+    public abstract class Stackable<T> {
+        public T baseValue;
+
+        public abstract T Value {
+            get;
+        }
+
+        protected List<Modifier<T>> modifiers;
+        public Modifier<T> AddModifier(T value) {
+            modifiers ??= new List<Modifier<T>>();
+
+            var handle = new Modifier<T>() {
+                value = value
+            };
+            modifiers.Add(handle);
+            return handle;
+        }
+
+        public void RemoveMultiplier(Modifier<T> handle) {
+            modifiers?.Remove(handle);
+        }
+    }
+}
