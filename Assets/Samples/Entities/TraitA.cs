b724b3123dea32f4f69a5e20aa80fd144818205d
BrunoSilvaFreire <brunorbsf@gmail.com>
2021-04-22 10:40:02 -0300

    > Allowing Traits to be hidden by category

---
+++ Assets/Samples/Entities/TraitA.cs
@@ -0,0 +1,34 @@
+﻿using Lunari.Tsuki.Entities;
+
+namespace Lunari.Tsuki.Samples.Entities {
+    [TraitLocation(CommonLocation.View)]
+    public class TraitA : Trait {
+        
+    }
+    [TraitLocation(CommonLocation.Attack)]
+    public class TraitB : Trait {
+        
+    }
+    [TraitLocation(CommonLocation.AI)]
+    public class TraitC : Trait {
+        
+    }
+    [TraitLocation(CommonLocation.AI)]
+    public class AITraitThatDoStuff : Trait {
+        
+    }
+
+    [TraitLocation(CommonLocation.View)]
+    public class SpriteFlipper : Trait {
+        
+    }
+
+    [TraitLocation(CommonLocation.Combat)]
+    public class Combatant : Trait {
+        
+    }
+    [TraitLocation(CommonLocation.Combat)]
+    public class Combatant2 : Trait {
+        
+    }
+}
