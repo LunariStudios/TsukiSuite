3c691664a4b4f460bc6c89fcbb35745c6382f9c1
Bruno Silva Freire <brunorbsf@gmail.com>
2021-05-15 15:11:23 -0400

    Allow to get traits through subclasses

--- Entities/TraitDependencies.cs
+++ Entities/TraitDependencies.cs
@@ -4,18 +4,16 @@ using System.Linq;
 using Lunari.Tsuki.Entities.Problems;
 using Lunari.Tsuki.Runtime;
 using UnityEngine;
-
 #if UNITY_EDITOR
 using UnityEditor.Animations;
+
 #endif
 
 namespace Lunari.Tsuki.Entities {
     public class TraitDependencies : Traits {
         private readonly Trait of;
 
-        public List<Problem> Problems {
-            get;
-        } = new List<Problem>();
+        public List<Problem> Problems { get; } = new List<Problem>();
 
         public TraitDependencies(Entity entity, Trait of, bool initialize) {
             Entity = entity;
@@ -25,34 +23,31 @@ namespace Lunari.Tsuki.Entities {
 
         public bool Successful => Initialize && Problems.IsEmpty();
 
-        public bool Initialize {
-            get;
-        }
+        public bool Initialize { get; }
 
-        public Entity Entity {
-            get;
-        }
+        public Entity Entity { get; }
 
         public string FailureReason {
-            get {
-                return string.Join(Environment.NewLine, Problems.Select(exception => exception.Description));
-            }
+            get { return string.Join(Environment.NewLine, Problems.Select(exception => exception.Description)); }
         }
 
-        public T DependsOn<T>() where T : Trait {
-            var found = GetTrait<T>();
+        public T DependsOn<T>(bool allowSubclasses = false) where T : Trait {
+            var found = GetTrait<T>(allowSubclasses);
             if (found == null) {
                 Problems.Add(new MissingTrait(Entity, typeof(T), of));
             }
+
             return found;
         }
+
         public T RequiresComponent<T>(string expectedLocation = null) where T : Component {
             if (!Entity.TryGetComponentInChildren(out T component)) {
                 Problems.Add(new MissingComponent(Entity, typeof(T), of, expectedLocation));
             }
+
             return component;
         }
- 
+
         public void RequiresAnimatorParameter(string parameter, AnimatorControllerParameterType type) {
             var animator = RequiresComponent<Animator>();
             if (animator != null && animator.runtimeAnimatorController == null) {
@@ -62,9 +57,11 @@ namespace Lunari.Tsuki.Entities {
             }
 #if UNITY_EDITOR
             if (animator != null && animator.runtimeAnimatorController is AnimatorController controller) {
-                if (controller.parameters.Any(controllerParameter => controllerParameter.name == parameter && controllerParameter.type == type)) {
+                if (controller.parameters.Any(controllerParameter =>
+                    controllerParameter.name == parameter && controllerParameter.type == type)) {
                     return;
                 }
+
                 Problems.Add(new MissingAnimatorParameter(Entity, parameter, type, of));
             }
 #endif
