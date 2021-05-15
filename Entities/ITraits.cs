cece7e1c5c656ae680f9bf77bc4f83ddc376a469
Bruno Silva Freire <brunorbsf@gmail.com>
2021-05-15 15:11:23 -0400

    Allow to get traits through subclasses

--- Entities/ITraits.cs
+++ Entities/ITraits.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Lunari.Tsuki.Entities {
     public interface ITraits {
-        T GetTrait<T>() where T : Trait;
+        T GetTrait<T>(bool allowSubclass = false) where T : Trait;
         IEnumerable<T> GetTraits<T>() where T : Trait;
     }
 }
--- Entities/Traits.cs
+++ Entities/Traits.cs
@@ -1,17 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 namespace Lunari.Tsuki.Entities {
     public class Traits : ITraits {
         private readonly Dictionary<int, Trait> traits = new Dictionary<int, Trait>();
-        public T GetTrait<T>() where T : Trait {
-            if (traits.TryGetValue(typeof(T).GetHashCode(), out var found)) {
-                if (found is T cast) {
-                    return cast;
+
+        public T GetTrait<T>(bool allowSubclass = false) where T : Trait {
+            if (allowSubclass) {
+                foreach (var trait in traits.Values) {
+                    if (trait is T required) {
+                        return required;
+                    }
+                }
+            } else {
+                if (traits.TryGetValue(typeof(T).GetHashCode(), out var found)) {
+                    if (found is T cast) {
+                        return cast;
+                    }
                 }
             }
+
             return null;
         }
+
         public IEnumerable<T> GetTraits<T>() where T : Trait {
             foreach (var keyValuePair in traits) {
                 var trait = keyValuePair.Value;
@@ -20,17 +32,19 @@ namespace Lunari.Tsuki.Entities {
                 }
             }
         }
+
         public bool HasTrait(Type type) {
             return traits.Any(pair => pair.Key == type.GetHashCode());
         }
+
         public void Include(Trait trait) {
             traits[trait.GetType().GetHashCode()] = trait;
         }
+
         public void Include(Traits traits) {
             foreach (var pair in traits.traits) {
                 Include(pair.Value);
             }
         }
     }
-
 }
