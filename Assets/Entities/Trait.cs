1d61df6eacf30c284970e6681b1210d789bc9aa4
Bruno Silva Freire <brunorbsf@gmail.com>
2021-05-31 12:41:18 -0400

    Rename TraitDependencies to TraitDescriptor

--- Assets/Entities/Trait.cs
+++ Assets/Entities/Trait.cs
@@ -4,12 +4,12 @@ namespace Lunari.Tsuki.Entities {
         private Entity entity;
 
         public Entity Owner => entity;
-        public TraitDependencies TryClaim(Entity requisitor, Trait[] traits, bool initialize = true) {
+        public TraitDescriptor TryClaim(Entity requisitor, Trait[] traits, bool initialize = true) {
             if (initialize && entity != null) {
                 return null;
             }
 
-            var claims = new TraitDependencies(requisitor, this, initialize);
+            var claims = new TraitDescriptor(requisitor, this, initialize);
             foreach (var trait in traits) {
                 claims.Include(trait);
             }
@@ -19,10 +19,10 @@ namespace Lunari.Tsuki.Entities {
             }
             return claims;
         }
-        public bool TryClaim(Entity requisitor, Trait[] traits, out TraitDependencies dependencies, bool initialize = true) {
-            var result = dependencies = TryClaim(requisitor, traits, initialize);
+        public bool TryClaim(Entity requisitor, Trait[] traits, out TraitDescriptor descriptor, bool initialize = true) {
+            var result = descriptor = TryClaim(requisitor, traits, initialize);
             return result != null && result.Successful;
         }
-        public virtual void Configure(TraitDependencies dependencies) { }
+        public virtual void Configure(TraitDescriptor descriptor) { }
     }
 }
