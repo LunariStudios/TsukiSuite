1434381d1c11d80bf734638be8b282a4c2ffc771
Bruno Silva Freire <brunorbsf@gmail.com>
2022-02-01 13:28:43 -0300

    > Starting on support for netcode

--- Entities/Problems/MissingAnimatorParameter.cs
+++ Entities/Problems/MissingAnimatorParameter.cs
@@ -8,7 +8,7 @@ namespace Lunari.Tsuki.Entities.Problems {
             Entity entity,
             string name,
             AnimatorControllerParameterType parameterType,
-            Trait requisitor
+            ITrait requisitor
         ) : base(requisitor, entity, $"Entity {entity.name}'s animator not have a parameter named {name} of type {parameterType} which is a dependency of {requisitor} ") {
             ParameterName = name;
             ParameterType = parameterType;
