623c66ba081e194d89e57f3be5de2c479448788e
Bruno Silva Freire <brunorbsf@gmail.com>
2022-02-09 14:18:49 -0300

    > Added compile definitions for when entities is present

---
+++ Entities/Editor/CompileDefinitions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+namespace Lunari.Tsuki.Entities.Editor {
+    public static class CompileDefinitions {
+        public const string EntitiesCompileDefinition = "TSUKI_ENTITIES";
+        [InitializeOnLoadMethod]
+        public static void EnsureHasDefinitions() {
+            var group = EditorUserBuildSettings.selectedBuildTargetGroup;
+            var definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
+            var definitions = new HashSet<string>(definesString.Split(';'));
+            if (!definitions.Contains(EntitiesCompileDefinition)) {
+                definitions.Add(EntitiesCompileDefinition);
+            }
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(
+                group,
+                definitions.ToArray()
+            );
+        }
+    }
+}
