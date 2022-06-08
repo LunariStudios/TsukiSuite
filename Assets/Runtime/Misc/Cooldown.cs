6ed737a06056dae0d2dadb1d6d4347dc5de2eb51
Bruno Silva Freire <brunorbsf@gmail.com>
2022-06-08 14:31:45 -0300

    > Adding cooldown and more trait options

---
+++ Assets/Runtime/Misc/Cooldown.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+namespace Lunari.Tsuki.Misc {
+    [Serializable]
+    public class Cooldown {
+        private float _currentTime = 0;
+        public float cooldown = 1;
+
+        public void Use() {
+            _currentTime = cooldown;
+        }
+
+        public bool Tick() {
+            _currentTime -= Time.deltaTime;
+            if (_currentTime < 0) {
+                _currentTime = 0;
+                return true;
+            }
+            return Available;
+        }
+
+        public bool Available => _currentTime <= 0;
+
+        public WaitUntil WaitUntilReady() {
+            return new WaitUntil(Tick);
+        }
+        public IEnumerator UseAndWait() {
+            Use();
+            yield return WaitUntilReady();
+        }
+    }
+}
