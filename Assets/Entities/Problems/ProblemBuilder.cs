86625d1a1e36b6be05b4d3c73f93107325a15275
Bruno Silva Freire <brunorbsf@gmail.com>
2022-06-17 14:35:54 -0300

    > Adding more extension methods to TraitDescriptor.cs

--- Assets/Entities/Problems/ProblemBuilder.cs
+++ Assets/Entities/Problems/ProblemBuilder.cs
@@ -2,17 +2,19 @@ using System;
 namespace Lunari.Tsuki.Entities.Problems {
     public class ProblemBuilder {
         private TraitDescriptor descriptor;
-        private Problem problem;
         public ProblemBuilder(TraitDescriptor descriptor, string description) {
             this.descriptor = descriptor;
-            problem = new Problem(descriptor.Of, descriptor.Entity, description);
+            Problem = new Problem(descriptor.Of, descriptor.Entity, description);
         }
+
+        public Problem Problem { get; }
+
         public ProblemBuilder WithSolution(Solution solution) {
-            problem.WithSolution(solution);
+            Problem.WithSolution(solution);
             return this;
         }
         public ProblemBuilder WithSolution(string description, Action solution) {
-            problem.WithSolution(description, solution);
+            Problem.WithSolution(description, solution);
             return this;
         }
     }
