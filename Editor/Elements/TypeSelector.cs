ac7ef42ef69a34fbfda301bfb84de611f066dc33
Bruno Silva Freire <brunorbsf@gmail.com>
2022-11-17 10:23:42 -0300

    Add TypeSelector UIElement

---
+++ Editor/Elements/TypeSelector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Lunari.Tsuki.Editor.Elements {
+    public class TypeSelector<T> : VisualElement {
+        private readonly PopupField<Type> _dropdown;
+        public event Action<Type> OnTypeSelected;
+        public TypeSelector() {
+            var types = TypeCache.GetTypesDerivedFrom<T>()
+                .Where(type => !type.IsAbstract)
+                .ToList();
+
+            _dropdown = new PopupField<Type>() {
+                choices = types,
+                formatSelectedValueCallback = FormatLabel
+            };
+            _dropdown.RegisterValueChangedCallback(OnChanged);
+            Add(_dropdown);
+
+        }
+        private void OnChanged(ChangeEvent<Type> evt) {
+            var type = evt.newValue;
+            if (type != null) {
+                OnTypeSelected?.Invoke(type);
+            }
+        }
+        private string FormatLabel(Type arg) {
+            if (arg == null) {
+                return string.Empty;
+            }
+            return arg.GetLegibleName();
+        }
+
+        public string title {
+            get => _dropdown.label;
+            set => _dropdown.label = value;
+        }
+
+    }
+}
